263ac97f5f0994531b60a3eb0cf688f934150b3e
prathikreddy1 <131741909+prathikreddy1@users.noreply.github.com>
2025-07-16 21:23:32 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#0d0d0d',
+      },
+    },
+  },
+  plugins: [],
+}
